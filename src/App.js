04cd5272fbe040211d15cdfc404b913b6877c465
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2022-01-11 21:16:42 -0500

    Add api calls. Test ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./App.css"
+import getArticles from "./apiCalls"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [articles, setArticles] = useState([])
+  const [errorMsg, setErrorMsg] = useState("")
+
+  useEffect(() => {
+    getArticles("home")
+      .then(data => setArticles([...data.results]))
+      .catch(error => setErrorMsg(error))
+  }, [])
+
   return <div className="App">Hello world</div>
 }
 
